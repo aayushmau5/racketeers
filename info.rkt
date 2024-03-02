fe742accf358a034f80be6c873becdf129a05962
Aayush Kumar Sahu <aayushmau5@gmail.com>
2024-03-02 21:27:57 +0530

    update deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racketeers")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "gui-lib"))
 (define scribblings '(("scribblings/racketeers.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
